0f2eca14ac6f75baa2c9588199153da33d5078d5
Franz Xaver Schmid <42220771+franzxschmid@users.noreply.github.com>
2019-06-09 22:14:35 +0200

    Added Builder

--- CarManagement/src/de/gfn/carmanagement/entity/CarModel.java
+++ CarManagement/src/de/gfn/carmanagement/entity/CarModel.java
@@ -5,11 +5,20 @@ package de.gfn.carmanagement.entity;
 public class CarModel extends AbstractEntity{
     
     private String name;
-    
     private String brand;
-    
     private int price;
 
+    //Default Konstruktor
+    public CarModel(){}
+    
+    //Konstruktor unter Verwendung des Builders
+    public CarModel(CarModelBuilder builder){
+        this.name = builder.name;
+        this.brand = builder.brand;
+        this.price = builder.price;
+    }
+    
+    //Getter und Setter
     public String getName() {
         return name;
     }
@@ -34,5 +43,35 @@ public class CarModel extends AbstractEntity{
         this.price = price;
     }
     
-    
+    //Builder Klasse
+    public static class CarModelBuilder{
+        
+        private String name;
+        private String brand;
+        private int price;
+        
+        //Konstruktor Builder
+        public CarModelBuilder(){}
+        
+        //With und Build Methoden
+        public CarModelBuilder withName(String name){
+            this.name = name;
+            return this;
+        }
+        
+        public CarModelBuilder withBrand(String brand){
+            this.brand = brand;
+            return this;
+        }
+        
+        public CarModelBuilder withPrice(int price){
+            this.price = price;
+            return this;
+        }
+        
+        public CarModel build(){
+            return new CarModel(this);
+        }
+
+    }
 }
