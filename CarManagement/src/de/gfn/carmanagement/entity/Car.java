a8c692c914317bb04693983513c9c8317400ca51
Franz Xaver Schmid <42220771+franzxschmid@users.noreply.github.com>
2019-06-09 22:13:19 +0200

    Added Builder

--- CarManagement/src/de/gfn/carmanagement/entity/Car.java
+++ CarManagement/src/de/gfn/carmanagement/entity/Car.java
@@ -1,11 +1,22 @@
 package de.gfn.carmanagement.entity;
 
-//Beschreibt die Entität "car"
+
 public class Car extends AbstractEntity {
     
+    //Attribute
     private String registration;
     private String brand;
 
+    //Default Konstruktor
+    public Car(){};
+    
+    //Konstruktor der den Builder nutzt
+    public Car(CarBuilder builder){
+        this.registration = builder.registration;
+        this.brand = builder.brand; 
+    }
+    
+    //Getter und Setter
     public String getRegistration() {
         return registration;
     }
@@ -21,4 +32,29 @@ public class Car extends AbstractEntity {
     public void setBrand(String brand) {
         this.brand = brand;
     }
+    
+    //Builder
+    public static class CarBuilder{
+           
+        private String registration;
+        private String brand;
+        
+        //Builder Konstruktor
+        public CarBuilder(String registration){
+            this.registration = registration;
+        }
+        
+        //With-Methode
+        public CarBuilder withBrand(String brand){
+            this.brand = brand;
+            return this;
+        }
+        
+        //Build-Methode
+        public Car build(){
+            return new Car(this);
+        }
+          
+    }
+
 }
