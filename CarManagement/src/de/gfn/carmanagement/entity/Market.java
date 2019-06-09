0bef00c8a088d74c11757eff10500571fce78fcb
Franz Xaver Schmid <42220771+franzxschmid@users.noreply.github.com>
2019-06-09 22:16:35 +0200

    Added Builder

--- CarManagement/src/de/gfn/carmanagement/entity/Market.java
+++ CarManagement/src/de/gfn/carmanagement/entity/Market.java
@@ -4,11 +4,19 @@ package de.gfn.carmanagement.entity;
 public class Market extends AbstractEntity {
     
     private String country;
-    
     private int inhabitants;
-    
     private int carsPerCapita;
 
+    //Konstruktor unter Verwendung Builder
+    public Market(MarketBuilder builder){
+        this.country = builder.country;
+        this.inhabitants = builder.inhabitants;
+        this.carsPerCapita = builder.carsPerCapita;
+    }
+
+    public Market() {}
+    
+    //Getter und Setter
     public String getCountry() {
         return country;
     }
@@ -33,6 +41,30 @@ public class Market extends AbstractEntity {
         this.carsPerCapita = carsPerCapita;
     }
     
-    
+    //Builder
+    public static class MarketBuilder{
+       
+        private String country;
+        private int inhabitants;
+        private int carsPerCapita;
+        
+        public MarketBuilder(String country){
+            this.country = country;
+        }
+        
+        //With und Build Methoden
+        public MarketBuilder withInhabitants(int inhabitants){
+            this.inhabitants = inhabitants;
+            return this;
+        }
+        
+        public MarketBuilder withCarsPerCapita(int carsPerCapita){
+            this.carsPerCapita = carsPerCapita;
+            return this;
+        }
+        
+        public Market build(){
+            return new Market(this);
+        }
+    }
 }
-
